bafaa2e33e985e6896b82f59a1d526b5d13be0f0
Wagner Büttner <wagner.buettner@gmail.com>
2022-12-13 18:06:11 +0100

    Initial Commit

---
+++ src/main/java/com/wagner/fleetmanager/FleetManagerApplication.java
@@ -0,0 +1,13 @@
+package com.wagner.fleetmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagner/fleetmanager/FleetManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagner.fleetmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
